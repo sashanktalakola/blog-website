babcb9dce901f3ce818cf74e044b9d2b0eb6a823
Sashank <sashanktalakola2@gmail.com>
2024-07-06 18:43:33 -0400

    reboot

--- webistes/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-admin.site.register(models.POST)
-admin.site.register(models.COMMENT)
-admin.site.register(models.TAG)
-admin.site.register(models.USER)
