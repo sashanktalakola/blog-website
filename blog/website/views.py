fe344b6bb79a0ed918b71fc00a02a4e5c4a98167
Sashank <sashanktalakola2@gmail.com>
2024-07-13 10:42:54 -0500

    added landing and 404 page

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+class Index(View):
+    def get(self, request):
+        return render(request, "website/index.html")
+    
+    def post(self, request):
+        return HttpResponse("POST Request")
+    
+class PageNF(View):
+    def get(self, request):
+        return render(request, "404.html")
